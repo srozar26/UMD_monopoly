822ae914017dce132e8ed396590826e9f7a1286b
Jonathan Valladares <jvallad1@terpmail.umd.edu>
2025-12-08 03:56:55 -0500

    a

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 from random import randint
-# still need board, player, and property import
+
+
+from board import MakeBoard       
+from property import Property
+
 from event_generator import event_generator
 from decision_engine import decision_engine
 from save import save_game
@@ -9,14 +13,14 @@ from save import save_game
 
 class Game:
    def __init__(self):
-       self.event = event_generator()
-       #add board and player attributes here
-       self.cpu = decision_engine()
-       self.save = save_game()
-       
-      
-      
-       self.turn_count = 0
+        self.board = MakeBoard()
+        self.player = Player("Testudo")
+        self.cpu = Player("CPU")  # CPU player added
+        self.board.players["@"] = 0     # human player
+        self.board.players["#"] = 20    # cpu 
+        self.current_player = "player"
+        self.turn_count = 0
+        self.turn_count_max = 5  # stops infinite rolling
        
       
    def take_turn(self):
@@ -26,14 +30,22 @@ class Game:
       
        roll = randint(1,6)
        print("\nYou rolled a", roll)
+       
+         # move player along the edge path
+        self.player.move(roll, 40)  # 40 because 11+11+9+9 for perimeter of board
+        self.board.players["@"] = self.player.position
+
+        # Display current board
+        self.board.display_board()
+        
+        
+        # tile the player landed on
+        row, col = self.board.position_to_coords(self.player.position)
+        tile_symbol = self.board.board_layout[row].split()[col]
+        
+         print(f"You landed on tile symbol: {tile_symbol}")
       
-       """
-       the 2 lines below may need to be changed depending on what the code for the player and board files have for names.
-       For right now, this is what i put to deal with a player moving x amount of spaces depending on roll and where they will land in respect to the board
-       """
-       self.player.move(roll, self.board.size())
-       tile = self.board.get_tile(self.player.position)
-      
+
        # make code for if the space is the starting spot
       
        # make code for if the space has an event
@@ -47,13 +59,14 @@ class Game:
       
        # code for if a player is out of money (game over)
        
-       
+
     
        if self.player.cash <= 0:
            print("You hare out of money. Game over.")
            return False
        return True
    
+
    
     def cpu_take_turn(self):
     self.turn_count += 1
@@ -90,7 +103,21 @@ class Game:
         "Can implement after each turn "
         return f"Current Player:{self.current_player}\nBoard:{self.Board}"
       
-  
+    
+    def end_game(self): 
+        
+        """this gives each player what their cash and properties were."""
+        
+        print("\nGame over")
+        print("Total cash:", self.player.cash)
+        print("Properties owned:", self.player.properties)
+
+        save_game({
+            "cash": self.player.cash,
+            "properties": self.player.properties,
+            "turns": self.turn_count
+        })
+   
     if __name__ =="__main__":
    #code here should be to run the game and then once done just type in terminal 'python game.py'
     
